600831ddc507f8a71d505adee3709e3de35ce74d
laminemessaci <lamine.messaci18@gmail.com>
2023-02-20 19:17:52 +0100

    Updt dropDown style

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -19,7 +19,7 @@ export default function Dropdown() {
     <div className='w-auto text-right justify-center   rounded-full '>
       <Menu as='div' className='relative inline-block text-left'>
         <div>
-          <Menu.Button className='inline-flex w-full justify-center rounded-full items-center  bg-black bg-opacity-20 px-2 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+          <Menu.Button className='inline-flex w-full justify-center rounded-full items-center  bg-lime-900 bg-opacity-20 px-2 py-2 text-sm font-medium text-white hover:bg-opacity-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
             <UserAvatar /> Options
             <ChevronDownIcon className='ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100' aria-hidden='true' />
           </Menu.Button>
