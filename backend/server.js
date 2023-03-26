2bbf52a34040079dc031f20169d0fa9cbfd889bc
laminemessaci <lamine.messaci18@gmail.com>
2023-03-26 14:16:24 +0200

    predeploy

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,9 @@ app.use('/api/users', userRoutes);
 app.use('/api/upload', uploadRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
