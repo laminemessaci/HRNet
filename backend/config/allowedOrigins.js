13244db4a581d29d4fa277df0be3742658314ad1
laminemessaci <lamine.messaci18@gmail.com>
2023-10-19 21:01:47 +0200

    Update Origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,10 +1,11 @@
 const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:3500',
-  'http://127.0.0.1:3500',
-  'http://localhost:5000',
+  "http://localhost:3000",
+  "http://localhost:3500",
+  "http://127.0.0.1:3500",
+  "http://localhost:5000",
 
-  'http://127.0.0.1:5000',
+  "http://127.0.0.1:5000",
+  "https://hr-net-vqtc.vercel.app/",
 ];
 
 export default allowedOrigins;
