6e8373a0ce8a0cadaa7e71bbd183395dd35e302f
laminemessaci <lamine.messaci18@gmail.com>
2023-10-19 22:02:26 +0200

    prepare for prod

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,11 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3500",
-  "http://127.0.0.1:3500",
-  "http://localhost:5000",
-
-  "http://127.0.0.1:5000",
-  "http://hr-net-vqtc.vercel.app",
-];
+const allowedOrigins = ["https://todo.com"];
 
 export default allowedOrigins;
