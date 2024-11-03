9048524dae9b4ec94cbdf68b2bab44ff135742d6
lamminemessaci <lamine.messaci18@gmail.com>
2024-11-03 18:28:48 +0100

    Add new origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://crm-ejqj.onrender.com",
   "http://127.0.0.1:5000",
+  "https://gismic-dash.onrender.com",
 ];
 
 export default allowedOrigins;
